0a113fe627d03800184651395b86ba1be9575294
talef <talefraim96@gmail.com>
2023-06-09 16:59:56 +0300

    Logger added.

--- Camera_module.py
+++ Camera_module.py
@@ -1,30 +1,7 @@
-import cv2
-import Logger_module
-
-def control_camera(on_off):
-    if on_off == "on":
-        cap = cv2.VideoCapture(0)
-        if not cap.isOpened():
-            ErrorMsg = 'Failed to open camera.'
-            Logger_module.Add_Trace_To_Logfile(message=ErrorMsg, log_mode='ERROR')
-            return
-        # Read frames from the camera while the camera is on
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            # Display the frame
-            cv2.imshow("Camera", frame)
-            # Check if the camera should be turned off
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        # Release the camera and close the window
-        cap.release()
-        cv2.destroyAllWindows()
-    elif on_off == "off":
-        # Close any open camera windows
-        cv2.destroyAllWindows()
-    else:
-        print("Invalid on_off argument. Please specify either 'on' or 'off'.")
 
 
+def Turn_camera_on():
+    return
+
+def Turn_camera_off():
+    return
--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import sys
 from FrontEnd_module import Ui_MainWindow
-import Logger_module
-import Camera_module
-from functools import partial
+import Logger_module, Camera_module
+from PyQt5.QtMultimedia import *
+from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtWidgets as Qtw
 
 
@@ -11,9 +11,14 @@ class UI(Qtw.QMainWindow):
         super(UI, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.Camera_on = False
-        self.ui.TurnCameraButton.clicked.connect(partial(self.Camera_button_pressed, "on"))
-        self.ui.TurnCameraButton.clicked.connect(partial(self.Camera_button_pressed, "off"))
+        self.Available_cameras = QCameraInfo.availableCameras()
+        if not self.available_cameras:
+            self.Available_cameras = 0
+            WarningMsg = "No available cameras."
+            Logger_module.Add_Trace_To_Logfile(WarningMsg, "WARNING")
+        self.Video_capture = False
+        self.ui.TurnCameraButton.clicked.connect(self.Camera_button_pressed)
+        self.ui.TurnCameraButton.clicked.connect(self.Camera_button_pressed)
         self.ui.SetExportPathButton.clicked.connect(self.Set_export_path)
         self.ui.CreateReportButton.clicked.connect(self.Create_report)
 
@@ -24,17 +29,20 @@ class UI(Qtw.QMainWindow):
     def Create_report(self):
         return
 
-
+    @pyqtSlot()
     def Camera_button_pressed(self):
         try:
-            if not self.Camera_on:
-                self.Camera_on = True
+            if self.Available_cameras != 0 and not self.Video_capture:
+                self.Video_capture = True
                 Msg = "Camera turned on"
                 Logger_module.Add_Trace_To_Logfile(message=Msg, log_mode='INFO')
-            else:
-                self.Camera_on = False
+                Camera_module.Turn_camera_on()
+
+            elif self.Available_cameras != 0 and self.Video_capture:
+                self.Video_capture = False
                 Msg = "Camera turned off"
                 Logger_module.Add_Trace_To_Logfile(message=Msg, log_mode='INFO')
+                Camera_module.Turn_camera_off()
 
         except Exception as ErrorMsg:
             Logger_module.Add_Trace_To_Logfile(message=ErrorMsg, log_mode=ErrorMsg)
