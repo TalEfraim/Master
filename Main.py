f9d95e67508e4eedecf4c119604de4e18350f2d3
talef <talefraim96@gmail.com>
2023-06-13 22:58:01 +0300

    General fixings.

--- Main.py
+++ Main.py
@@ -1,15 +1,22 @@
 import os
 import sys
+import cv2
 from PyQt5 import QtWidgets as Qtw
 from natsort import natsorted, ns, index_natsorted, order_by_index
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import *
 from PyQt5.QtMultimedia import *
 from PyQt5.QtWidgets import QLabel, QFileDialog, QAction
-import Camera_module
 import Logger_module
-import Database_module as DATABASE
 from FrontEnd_module import Ui_MainWindow
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QMessageBox
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt, QMutex, QWaitCondition
+import cv2
+import sys
+import time
 
 # ==================================================================================================================== #
 # TODO: Software front-end / back-end tasks:
@@ -33,13 +40,48 @@ from FrontEnd_module import Ui_MainWindow
 # ==================================================================================================================== #
 
 
+class CamThread(QThread):
+    changemap = pyqtSignal('QImage')
+
+    def __init__(self, mutex, condition):
+        super().__init__()
+        self.mutex = mutex
+        self.condition = condition
+        self.running = True
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        while self.running:
+            try:
+                ret, img_rgb = cap.read()
+                if ret:
+                    rgb = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB)
+
+                    #any other image processing here
+
+                    convert = QImage(rgb.data, rgb.shape[1], rgb.shape[0], QImage.Format_RGB888)
+                    p = convert.scaled(640, 480, Qt.KeepAspectRatio)
+                    self.changemap.emit(p)
+                    self.condition.wait(self.mutex)
+
+            except Exception as ErrorMsg:
+                Logger_module.Add_Trace_To_Logfile(message=ErrorMsg, log_mode='ERROR')
+
+    def stop(self):
+        self.running = False
+
+
 class UI(Qtw.QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
-        self.selected_camera_index = 0
-        self.ImagesList = None
+        self.mutex = QMutex()
+        self.condition = QWaitCondition()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.selected_camera_index = 0
+        self.ImagesList = None
         self.SoftwareVersion = '1.0.0'
         self.ui.actionversion_number.setText(self.SoftwareVersion)
         self.Available_cameras = QCameraInfo.availableCameras()
@@ -47,38 +89,56 @@ class UI(Qtw.QMainWindow):
             self.Available_cameras = 0
             WarningMsg = "No available cameras."
             Logger_module.Add_Trace_To_Logfile(WarningMsg, "WARNING")
-        elif len(self.Available_cameras) > 1:
+        elif len(self.Available_cameras) > 0:
             for camera in self.Available_cameras:
                 action = QAction(camera.description(), self.ui.menuChoose_camera)
                 action.triggered.connect(lambda _, cam=camera: self.select_camera(cam))
                 self.ui.menuChoose_camera.addAction(action)
-            # TODO: write a method to set the available devices to the menu bar.
-            # return
-            print(self.Available_cameras)
         self.Video_capture = False
         self.output_directory_path = None
         self.ui.actionLoad_target_images.triggered.connect(self.Load_Dataset)
         self.ui.CurrentImage_slider.valueChanged.connect(self.Value_changed_ImageSlider)
         self.ui.CurrentImage_radiobox.valueChanged.connect(self.Value_changed_ImageRadiobox)
         self.ui.TurnCameraButton.clicked.connect(self.Camera_button_pressed)
-        self.ui.TurnCameraButton.clicked.connect(self.Camera_button_pressed)
         self.ui.SetExportPathButton.clicked.connect(self.Set_export_path)
         self.ui.CreateReportButton.clicked.connect(self.Create_report)
-        self.Worker1 = Camera_module.Worker1(WhichCameraIndex=self.selected_camera_index)
-        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+
+
+    def InitCamera(self):
+        self.mutex.lock()
+        self.thr = CamThread(mutex=self.mutex,condition=self.condition)
+        self.thr.changemap.connect(self.ImageUpdateSlot)
+        self.thr.start()
+
+    def StopCamera(self):
+        if self.thr:
+            self.thr.stop()
+            self.thr.wait()  # Wait for the thread to finish before continuing
+        self.mutex.unlock()
 
     def select_camera(self, camera):
         selected_index = QCameraInfo.availableCameras().index(camera)
         self.selected_camera_index = selected_index
-        print(self.selected_camera_index)
 
+    @pyqtSlot('QImage')
     def ImageUpdateSlot(self, Image):
-        self.ui.MainVideo.setPixmap(QPixmap.fromImage(Image))
+        self.mutex.lock()
+        try:
+            self.ui.MainVideo.setPixmap(QPixmap.fromImage(Image))  # I think this line causing the crash after video stop.
+        finally:
+            self.mutex.unlock()
+            self.condition.wakeAll()
 
+        # except Exception as ErrorMsg:
+        #     Logger_module.Add_Trace_To_Logfile(message=ErrorMsg, log_mode='ERROR')
 
     def Load_Dataset(self):
         Input_folder = QFileDialog.getExistingDirectory(self, "Please choose images location directory.")
         self.ImagesList = [f for f in os.listdir(Input_folder) if f.endswith('.jpg')]
+        for image in self.ImagesList:
+            pixmap = QPixmap(Input_folder+image)
+            self.ui.DatasetImages.setPixmap(pixmap)
+            self.ui.DatasetImages.setScaledContents(True)
         # TODO: update images on label.
 
 
@@ -125,20 +185,21 @@ class UI(Qtw.QMainWindow):
     def Create_report(self):
         return
 
-    # @pyqtSlot()
+    @pyqtSlot()
     def Camera_button_pressed(self):
         try:
             if self.Available_cameras != 0 and not self.Video_capture:
                 self.Video_capture = True
                 Msg = "Camera turned on"
                 Logger_module.Add_Trace_To_Logfile(message=Msg, log_mode='INFO')
-                Camera_module.Turn_camera_on(self, WhichCameraIndex=self.selected_camera_index)
+                self.InitCamera()
 
             elif self.Available_cameras != 0 and self.Video_capture:
                 self.Video_capture = False
                 Msg = "Camera turned off"
                 Logger_module.Add_Trace_To_Logfile(message=Msg, log_mode='INFO')
-                Camera_module.Turn_camera_off(self)
+                # self.thr.changemap.disconnect(self.ImageUpdateSlot)
+                self.StopCamera()
 
         except Exception as ErrorMsg:
             Logger_module.Add_Trace_To_Logfile(message=ErrorMsg, log_mode=ErrorMsg)
@@ -150,3 +211,6 @@ if __name__ == '__main__':
     window = UI()  # Create an instance of our class.
     window.show()
     app.exec_()  # Start the application.
+
+
+
